e571e43ef227aa65361cc70eb59c5497e9b45504
= <=>
2022-07-19 00:20:27 +0700

    add dashboard page

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,8 +1,14 @@
 // import { useSelector } from "react-redux"
 
+import DaftarTransaksi from "../components/DaftarTransaksi"
+import SideBar from "../components/SideBar"
+
 const Dashboard = () => {
-  return <div className="p-3">
-    <h1>dashboard</h1>
+  return <div className="h-[100vh] w-[100vw] flex justify-between bg-gray-50">
+    <SideBar />
+    <div className="w-full md:w-[85vw]">
+      <DaftarTransaksi />
+    </div>
   </div>
 }
 
