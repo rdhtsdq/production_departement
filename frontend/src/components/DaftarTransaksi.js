b510c3e9a2dbe6564da85f2df1912e409cd2bd08
= <=>
2022-07-19 00:22:04 +0700

    add Dashboard page

---
+++ frontend/src/components/DaftarTransaksi.js
@@ -0,0 +1,43 @@
+const DaftarTransaksi = ({data}) => {
+  return <div className="space-y-5 bg-blue-50 h-full p-5">
+    <div className="flex items-center h-[5vh] w-full justify-between">
+      <h1 className="mb-3 font-semibold">Daftar Transaksi</h1>
+      <form className="divide-x-2">
+        <input type="text" className="px-1 py-1 w-[40vw] md:w-[20vw] md:px-4 md: md:py-1 rounded-l-lg" placeholder="Cari.."/>
+        <button className="bg-white px-3 py-1 rounded-r-lg">x</button>
+      </form>
+    </div>
+    <div className="overflow-auto rounded-lg shadow">
+      <table className="w-full">
+        <thead className="bg-gray-50 border-b-2 border-gray-200">
+          <tr>
+            <th className="p-3 text-sm font-semibold tracking-wide text-left">No</th>
+            <th className="p-3 text-sm font-semibold tracking-wide text-left">No Transfer</th>
+            <th className="p-3 text-sm font-semibold tracking-wide text-left">Tanggal</th>
+            <th className="p-3 text-sm font-semibold tracking-wide text-left">Nama Customer</th>
+            <th className="p-3 text-sm font-semibold tracking-wide text-left">Jumlah Barang</th>
+            <th className="p-3 text-sm font-semibold tracking-wide text-left">Sub Total</th>
+            <th className="p-3 text-sm font-semibold tracking-wide text-left">Diskon</th>
+            <th className="p-3 text-sm font-semibold tracking-wide text-left">Ongkir</th>
+            <th className="p-3 text-sm font-semibold tracking-wide text-left">Total</th>
+          </tr>
+        </thead>
+        <tbody className="divide-y divide-gray-100">
+          <tr className="bg-white">
+            <td className="p-3 text-sm text-gray-700 whitespace-nowrap">1</td>
+            <td className="p-3 text-sm text-gray-700 whitespace-nowrap">202101-0001</td>
+            <td className="p-3 text-sm text-gray-700 whitespace-nowrap">01-jan-2021</td>
+            <td className="p-3 text-sm text-gray-700 whitespace-nowrap">Cust A</td>
+            <td className="p-3 text-sm text-gray-700 whitespace-nowrap">3</td>
+            <td className="p-3 text-sm text-gray-700 whitespace-nowrap">250000</td>
+            <td className="p-3 text-sm text-gray-700 whitespace-nowrap">5000</td>
+            <td className="p-3 text-sm text-gray-700 whitespace-nowrap">15000</td>
+            <td className="p-3 text-sm text-gray-700 whitespace-nowrap">260000</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+}
+
+export default DaftarTransaksi
---
+++ frontend/src/components/SideBar.js
@@ -0,0 +1,8 @@
+const SideBar = () => {
+  return <nav className="hidden md:flex flex-col p-5 space-y-3 justify-between w-[15vw] h-full bg-blue-500 text-white">
+    <h1 className="font-semibold text-xl">Halo, Admin</h1>
+
+  </nav>
+}
+
+export default SideBar
